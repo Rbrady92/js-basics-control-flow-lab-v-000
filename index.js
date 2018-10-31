5f92ce13083c244bbdd60ae074eeed44cb49c117
Ryan Brady <ryanbrady369@gmail.com>
2018-10-31 22:36:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function scuberGreetingForFeet(distance) {
   if (distance > 2500) return 'No can do.'
   return 'I will gladly take your thirty bucks.'
 }
+
+function ternaryCheckCity(city) {
+  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
