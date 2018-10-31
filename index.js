8949e9ac3e279667a7119cf7627a746cc74ed2a7
Ryan Brady <ryanbrady369@gmail.com>
2018-10-31 22:35:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 function scuberGreetingForFeet(distance) {
   if (distance <= 400) return 'This one is on me!'
   if (distance > 2500) return 'No can do.'
-  return 'I will gladly take your 30 bucks.'
+  return 'I will gladly take your thirty bucks.'
 }
